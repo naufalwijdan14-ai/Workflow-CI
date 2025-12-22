3f72c1f63e800753e6fc893ae540ed5ffdc78fc5
naufalwijdan14-ai <naufalwijdan14@gmail.com>
2025-12-22 21:31:56 +0700

    Update modelling.py

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -1,60 +1,67 @@
 import pandas as pd
 import mlflow
 import mlflow.sklearn
-import dagshub
 import os
-import sys
+import argparse
+
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 
-# 1.  parameter dari MLProject
-n_estimators = int(sys.argv[1]) if len(sys.argv) > 1 else 100
-max_depth = int(sys.argv[2]) if len(sys.argv) > 2 else 5
 
-# 2. Inisialisasi DagsHub 
+# 1. Argument dari MLflow Project
 
-os.environ['DAGSHUB_NON_INTERACTIVE'] = '1'
-dagshub.init(
-    repo_owner='naufalwijdan14-ai', 
-    repo_name='Eksperimen_SML_Muhamad-Naufal-Wijdan', 
-    mlflow=True
-)
+parser = argparse.ArgumentParser()
+parser.add_argument("--n_estimators", type=int, default=100)
+parser.add_argument("--max_depth", type=int, default=5)
+args = parser.parse_args()
 
-# 3. Load Dataset (Pastikan file csv ada di folder yang sama)
+n_estimators = args.n_estimators
+max_depth = args.max_depth
+
+
+# 2. Load Dataset
+
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 data_path = os.path.join(BASE_DIR, "titanic_preprocessing.csv")
 
-if not os.path.exists(data_path):
-    # Fallback jika path absolute gagal
-    data_path = "titanic_preprocessing.csv" 
-
 df = pd.read_csv(data_path)
+
 X = df.drop("Survived", axis=1)
 y = df["Survived"]
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# 4. MLflow Tracking
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+
+
+# 3. MLflow Tracking
+
 mlflow.set_experiment("Titanic_CI_Workflow")
 
 with mlflow.start_run(run_name="CI_Automated_Run"):
-    # Training Model
-    model = RandomForestClassifier(n_estimators=n_estimators, max_depth=max_depth, random_state=42)
+    # Train model
+    model = RandomForestClassifier(
+        n_estimators=n_estimators,
+        max_depth=max_depth,
+        random_state=42
+    )
     model.fit(X_train, y_train)
-    
-    # Evaluasi
+
+    # Evaluate
     y_pred = model.predict(X_test)
     acc = accuracy_score(y_test, y_pred)
-    
-    # Log ke DagsHub
+
+    # Log params & metrics
     mlflow.log_param("n_estimators", n_estimators)
     mlflow.log_param("max_depth", max_depth)
     mlflow.log_metric("accuracy", acc)
-    
+
+    # Register model (INI PENTING UNTUK BUILD DOCKER)
     mlflow.sklearn.log_model(
-        sk_model=model, 
-        artifact_path="model", 
+        sk_model=model,
+        artifact_path="model",
         registered_model_name="Titanic_Project"
     )
-    
-    print(f"Berhasil! Akurasi Model: {acc}")
+
+    print(f" Training selesai | Accuracy: {acc:.4f}")
