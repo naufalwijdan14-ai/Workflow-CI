f8029c8f84a4db86b2884c16f4de4e713130222b
naufalwijdan14-ai <naufalwijdan14@gmail.com>
2025-12-23 01:11:34 +0700

    Update modelling.py

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -1,21 +1,23 @@
-import argparse
 import pandas as pd
 import mlflow
 import mlflow.sklearn
-import joblib
+import dagshub
 import os
-
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--n_estimators", type=int, default=100)
-parser.add_argument("--max_depth", type=int, default=5)
-args = parser.parse_args()
+dagshub.init(
+    repo_owner="naufalwijdan14-ai",
+    repo_name="Eksperimen_SML_Muhamad-Naufal-Wijdan",
+    mlflow=True
+)
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-data_path = os.path.join(BASE_DIR, "titanic_preprocessing.csv")
+data_path = os.path.join(BASE_DIR, "..", "preprocessing", "titanic_preprocessing.csv")
+
+if not os.path.exists(data_path):
+    data_path = "titanic_preprocessing.csv"
 
 df = pd.read_csv(data_path)
 
@@ -26,22 +28,17 @@ X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
 
-with mlflow.start_run(run_name="CI_Automated_Run"):
-    model = RandomForestClassifier(
-        n_estimators=args.n_estimators,
-        max_depth=args.max_depth,
-        random_state=42
-    )
+mlflow.set_experiment("Titanic_Baseline")
 
+with mlflow.start_run(run_name="Baseline_Model"):
+    model = RandomForestClassifier(n_estimators=100, random_state=42)
     model.fit(X_train, y_train)
-    acc = accuracy_score(y_test, model.predict(X_test))
+    y_pred = model.predict(X_test)
+    acc = accuracy_score(y_test, y_pred)
 
-    mlflow.log_param("n_estimators", args.n_estimators)
-    mlflow.log_param("max_depth", args.max_depth)
+    mlflow.log_param("model_type", "RandomForest")
+    mlflow.log_param("n_estimators", 100)
     mlflow.log_metric("accuracy", acc)
-
     mlflow.sklearn.log_model(model, "model")
 
-    joblib.dump(model, "model.pkl")
-
-    print(f"Accuracy: {acc}")
+    print(acc)
